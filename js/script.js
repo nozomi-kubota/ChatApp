aa79c028ceabf9824551b960c3b8ac832ab7fb20
nozomi-kubota <nozomi.kubota@mitsubishicorp.com>
2024-06-13 12:24:42 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  set,
+  onChildAdded,
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+/**
+ * Config = 機密情報です！！！
+ * この部分はGitHubに上げないこと！！！！！！！
+ */
+const firebaseConfig = {
+  apiKey: "AIzaSyDU8hsM0VX-xlLrlzYi56OiBFDAjjnEhRM",
+  authDomain: "gsmil07-83480.firebaseapp.com",
+  databaseURL: "https://gsmil07-83480-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gsmil07-83480",
+  storageBucket: "gsmil07-83480.appspot.com",
+  messagingSenderId: "964837400155",
+  appId: "1:964837400155:web:2329a19d0e284a8fbca97e",
+  measurementId: "G-3M2NJN9RV9"
+};
+
+// // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);//RealtimeDBに接続
+const dbRef = ref(database, "chat"); //RealtimeDB内の”chat”に格納
+
+//送信ボタン(id=send)を押したときの処理
+$("#send").on("click", function () {
+
+  //入力欄のデータを取得(データの取得⇒Val)
+  const userName = $('#userName').val();
+  const text = $("#text").val();
+
+  //送信データを”message”で定義：userNameとtextをオブジェクトにまとめる
+  const message = {
+    userName: userName,
+    text: text,
+  };
+  //【newPostRef】の定義：Firesbase Realtime Databaseにオブジェクト送信⇒push
+  const newPostRef = push(dbRef);
+  //上で定義したnewPostRef に”message”‥userName,text をセット 
+  set(newPostRef, message);
+});
+//指定した場所にデータが追加されたことを検知(新しく入っている情報がdataに格納されている)
+onChildAdded(dbRef, function (data) {
+  //追加されたデータをfirebaseから受け取り、分解
+  const message = data.val();
+  const key = data.key;
+  console.log(data, message, key);
+  //
+  let chatMsg = ` 
+    <div>
+      <div>${message.userName}</div>
+      <div>${message.text}</div>
+    </div>
+  `;
+
+  $("#output").append(chatMsg);
+});
+
+//リセットボタン(id=resetb)を押したときの処理
+$("#resetb").on("click", function () {
+  userName.value = '';
+  text.value = '';
+})
